92f3534e7238f79b9845075fc4d9500826031e7e
Raj Taware <142778820+Raj-Taware@users.noreply.github.com>
2024-11-18 23:57:46 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", function() {
     // Fade in effect for the main title
     const title = document.getElementById('main-title');
-    title.style.opacity = 1;
-};
+    title.style.opacity = 1; // Change opacity to 1
+});
